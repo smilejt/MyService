4078633611114764ac5f33e117be5befc0374dfa
龙逸 <smile_jt@qq.com>
2020-05-05 19:53:10 +0800

    1.创建淘宝服务

--- service-taoBao-impl/src/main/java/cn/laoshengle/taobao/impl/mapper/GoodsCategoryMapper.java
+++ service-taoBao-impl/src/main/java/cn/laoshengle/taobao/impl/mapper/GoodsCategoryMapper.java
@@ -4,6 +4,9 @@ import cn.laoshengle.taobao.impl.pojo.GoodsCategoryPojo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @createData: 2020-05-05 15:48
  * @author: LongJunTao
@@ -13,7 +16,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GoodsCategoryMapper {
 
+    /**
+     * 新增类目对象
+     *
+     * @param pojo 实体对象
+     * @return 受影响条数
+     */
     int insert(GoodsCategoryPojo pojo);
 
+    /**
+     * 新增非空类目
+     *
+     * @param pojo 实体对象
+     * @return 受影响条数
+     */
     int insertSelective(GoodsCategoryPojo pojo);
+
+    /**
+     * 根据条件查询类目
+     * @param params 查询条件
+     * @return 查询结果集
+     */
+    List<GoodsCategoryPojo> getGoodsCategory(Map<String,Object> params);
 }
--- service-taoBao-impl/src/main/java/cn/laoshengle/taobao/impl/service/TaoBaoFeaturedServiceImpl.java
+++ service-taoBao-impl/src/main/java/cn/laoshengle/taobao/impl/service/TaoBaoFeaturedServiceImpl.java
@@ -4,15 +4,18 @@ import cn.laoshengle.core.entity.GoodsOriginalDataEntity;
 import cn.laoshengle.core.service.taobao.TaoBaoFeaturedService;
 import cn.laoshengle.taobao.impl.mapper.GoodsCategoryMapper;
 import cn.laoshengle.taobao.impl.mapper.GoodsOriginalDataMapper;
+import cn.laoshengle.taobao.impl.pojo.GoodsCategoryPojo;
+import cn.laoshengle.taobao.impl.pojo.GoodsOriginalDataPojo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.*;
 
 /**
  * @description:
@@ -37,6 +40,37 @@ public class TaoBaoFeaturedServiceImpl implements TaoBaoFeaturedService {
     public void insertTaoBaoFeaturedByEveryDay(List<GoodsOriginalDataEntity> paramsList) {
         logger.info("[TaoBaoFeaturedServiceImpl].[insertTaoBaoFeaturedByEveryDay]------> In ParamsList Num = {}", paramsList.size());
 
+        //类目查询参数
+        Map<String,Object> tempMap = new HashMap<>();
+        List<GoodsCategoryPojo> resultCategory = goodsCategoryMapper.getGoodsCategory(tempMap);
+        Map<String,String> categoryMap = new HashMap<>();
+        if (resultCategory != null){
+            for (GoodsCategoryPojo pojo:resultCategory) {
+                categoryMap.put(pojo.getCategoryName(),pojo.getCategoryId());
+            }
+        }
 
+        if (!paramsList.isEmpty()){
+
+            List<GoodsOriginalDataPojo> paramList = new ArrayList<>();
+            GoodsOriginalDataPojo pojo;
+            int num = 0;
+
+            //循环参入的的商品列表
+            for (GoodsOriginalDataEntity entity : paramsList) {
+                pojo = new GoodsOriginalDataPojo();
+                BeanUtils.copyProperties(entity, pojo);
+
+                pojo.setSystemId(UUID.randomUUID().toString().replace("-",""));
+                pojo.setInsertTime(new Date());
+                if (categoryMap.containsKey(pojo.getGoodCategory())){
+                    pojo.setGoodCategoryId(categoryMap.get(pojo.getGoodCategory()));
+                }else {
+
+                }
+
+                paramList.add(pojo);
+            }
+        }
     }
 }
