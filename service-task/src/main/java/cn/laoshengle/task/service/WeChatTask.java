acdc98da8545a6b35d00227c4120df9a16826f4f
longjuntao <smile_jt@qq.com>
2020-08-06 10:45:28 +0800

    启用service-task定时服务定时获取微信公众号Token信息

--- service-task/src/main/java/cn/laoshengle/task/service/WeChatTask.java
+++ service-task/src/main/java/cn/laoshengle/task/service/WeChatTask.java
@@ -1,10 +1,19 @@
 package cn.laoshengle.task.service;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @description: 微信相关定时任务
@@ -17,21 +26,26 @@ public class WeChatTask {
 
     private static final Logger logger = LoggerFactory.getLogger(WeChatTask.class);
 
+    @Resource
+    private RestTemplate restTemplate;
+
+    private static final String URL = "https://laoshengle.cn/api/weChat/weChatService/getAccessToken";
 
     /**
-     * 每1小时55分钟获取一次微信AccessToken
+     * 每2小时获取一次微信AccessToken
      */
-    @Scheduled(cron = "0/3 * * * * ?")
+    @Scheduled(cron = "0 0 0/2 * * ?")
     public void getWeChatAccessToken() {
-        System.out.println("[WeChatTask].[getWeChatAccessToken]------> start");
         logger.info("[WeChatTask].[getWeChatAccessToken]------> start");
         //通过restTemplate
+        ResponseEntity<String> weChatTokenResult = restTemplate.getForEntity(URL, String.class);
+        if (!StringUtils.isEmpty(weChatTokenResult) && weChatTokenResult.getStatusCodeValue() == 200) {
+            logger.info("[WeChatTask].[getWeChatAccessToken]------> success");
+            logger.info("[WeChatTask].[getWeChatAccessToken]------> body = {}", weChatTokenResult.getBody());
+        } else {
+            logger.error("[WeChatTask].[getWeChatAccessToken]------> fail");
+            logger.error("[WeChatTask].[getWeChatAccessToken]------> weChatTokenResult = {}", JSON.toJSONString(weChatTokenResult));
+        }
     }
 
-    //        ResponseEntity<String> resultEntity = restTemplate.getForEntity(CommonConstant.TASK_GET_WE_CHAT_TOKEN_URL, String.class);
-//        if (CommonConstant.HTTP_REQUEST_SUCCESS_CODE == resultEntity.getStatusCodeValue()){
-//            logger.info("[WeChatTask].[getWeChatAccessToken]------> resultEntity.getBody() = {}",resultEntity.getBody());
-//        }else {
-//            logger.error("[WeChatTask].[getWeChatAccessToken]------> Failed to obtain token");
-//        }
 }
