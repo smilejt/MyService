94dfcdc9593532d18f6055c9b88f92ca4e03b41b
longjuntao <smile_jt@qq.com>
2020-08-08 15:31:20 +0800

    今天不想改了

---
+++ service-oAuth2/src/test/java/cn/laoshengle/oauth2/tests/PasswordEncoderTests.java
@@ -0,0 +1,33 @@
+package cn.laoshengle.oauth2.tests;
+
+import cn.laoshengle.oauth2.OAuthApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author longjuntao
+ * @description:
+ * @date 2020/8/8 14:03
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = OAuthApplication.class)
+public class PasswordEncoderTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(PasswordEncoderTests.class);
+
+    @Resource
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Test
+    public void testPasswordEncoder() {
+        logger.info("dashboard = {}", passwordEncoder.encode("dashboard"));
+        logger.info("portal = {}", passwordEncoder.encode("portal"));
+    }
+}
