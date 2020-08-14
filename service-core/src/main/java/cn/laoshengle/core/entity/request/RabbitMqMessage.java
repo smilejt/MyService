8b6bc77316961b127cb6212e58019d88d1b3f15b
longjuntao <smile_jt@qq.com>
2020-08-14 15:55:16 +0800

    忘记改了个啥

--- service-core/src/main/java/cn/laoshengle/core/entity/request/RabbitMqMessage.java
+++ service-core/src/main/java/cn/laoshengle/core/entity/request/RabbitMqMessage.java
@@ -2,7 +2,6 @@ package cn.laoshengle.core.entity.request;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 /**
@@ -14,9 +13,7 @@ import java.io.Serializable;
 public class RabbitMqMessage implements Serializable {
     private static final long serialVersionUID = -7511521675581590845L;
 
-    @NotBlank(message = "消息队列名称不能为空")
     private String queueName;
 
-    @NotBlank(message = "消息内容不能为空")
     private String context;
 }
--- service-message/src/main/java/cn/laoshengle/message/controller/RabbitMqController.java
+++ service-message/src/main/java/cn/laoshengle/message/controller/RabbitMqController.java
@@ -1,6 +1,8 @@
 package cn.laoshengle.message.controller;
 
+import cn.laoshengle.core.constant.CommonConstant;
 import cn.laoshengle.core.entity.JsonResult;
+import cn.laoshengle.core.entity.MiNiProjectBannerEntity;
 import cn.laoshengle.core.entity.request.RabbitMqMessage;
 import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
@@ -19,6 +21,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 /**
  * @author longjuntao
@@ -31,6 +34,10 @@ public class RabbitMqController {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMqController.class);
 
+    private static final String EX_CHANGE_MSG_ERROR = "exChangeMsgError";
+
+    private static final String ROUTING_KEY_ERROR = "routingKeyError";
+
     @Resource
     private RabbitAdmin rabbitAdmin;
 
@@ -41,13 +48,20 @@ public class RabbitMqController {
 
     @Bean
     public MessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate) {
-        return new RepublishMessageRecoverer(rabbitTemplate, "exChangeMsgError", "routingKeyError");
+        return new RepublishMessageRecoverer(rabbitTemplate, EX_CHANGE_MSG_ERROR, ROUTING_KEY_ERROR);
     }
 
     @PostMapping("sendMq")
     public JsonResult sendMq(@RequestBody RabbitMqMessage message) {
         logger.info("[RabbitMqController].[sendMq]------> Send RabbitMQ Message Start, message = {}", JSON.toJSONString(message));
         this.sendMessage(message.getQueueName(), message.getContext());
+        return JsonResult.buildSuccess();
+    }
+
+    @PostMapping("sendMqJsonTest")
+    public JsonResult sendMqJsonTest(@RequestBody MiNiProjectBannerEntity entity) {
+        logger.info("[RabbitMqController].[sendMqJsonTest]------> Send RabbitMQ Message Start, entity = {}", JSON.toJSONString(entity));
+        this.sendMessage(CommonConstant.QUEUE_NAME, JSON.toJSONString(entity));
         return JsonResult.buildSuccess();
     }
 
